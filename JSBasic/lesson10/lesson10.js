e4944641e32a968e41ef0d5a9d124276a7cd112e
Lê Minh Trí <trilm189@gmail.com>
2024-09-13 16:31:46 +0700

    lesson 10

---
+++ JSBasic/lesson10/lesson10.js
@@ -0,0 +1,11 @@
+console.log("Hello world")
+let a = 5;
+let b = 5;
+
+console.log("compare a,b: " , "a = ",a," b ",b,"result ",a===b)
+// === type , value 
+// == value 
+
+// Emply , null, undefined
+let c = null;
+console.log("c : ",c)
