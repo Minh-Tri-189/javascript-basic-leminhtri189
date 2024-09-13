e1e33e0d2cc46539f8adfed7ae3dd066ad21f8ee
Lê Minh Trí <trilm189@gmail.com>
2024-09-13 16:06:41 +0700

    lesson 9

---
+++ JSBasic/lesson9/lesson9.js
@@ -0,0 +1,12 @@
+console.log('Hello world from html')
+let leminhtri = {
+name : 'tri',
+age: 20
+}; //key : value 
+
+console.log("Check data object: ", leminhtri)
+console.log("My name: ", leminhtri.name)
+// arr = [item1, item2, item3]
+let b = ["item1","item2","item3"]
+console.log("1: ",b[0])
+let c = {}
